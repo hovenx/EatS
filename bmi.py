61af84936d824f7a6b6780dccac2d5dd32703c8d
screamingforhelp <jeremaihrhion.castro@cvsu.edu.ph>
2025-01-26 18:41:38 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -41,15 +41,15 @@ if st.button("Calculate BMI"):
     df.to_csv("bmi_data.csv", index=False)
     st.success("BMI logged successfully!")
     
-if bmi == "Underweight":
-    st.write("Suggestions: ")
+if bmi < 18.5:
+    st.write("Suggestions (Underweight): ")
     st.write("Consider increasing your calorie intake and consulting a doctor or nutritionist.")
     st.write("For extra calories, add extras to your meals, like nut butter on whole-grain bread or cheese in casseroles. For more calories and protein, you can also add liquid or dry milk to food. Soups and mashed potatoes are two examples.")
     st.write("Start eating five to six smaller meals throughout the day gradually. To identify when you might be hungry, try to pay attention to your body. Even if you're not really hungry, you might need to schedule mealtimes.")
     st.write("Drinks have the power to fill you up. If it applies to you, refrain from drinking right before or during meals. However, be sure to consume adequate amounts of liquids throughout the day.")
     st.write("By increasing your muscle mass, exercise—especially strength training—can aid in weight growth. Additionally, exercise may increase your hunger.")
-elif bmi == "Overweight" or bmi == "Obese":
-    st.write("Suggestions: ")
+elif bmi >= 25.0:
+    st.write("Suggestions (Overweight/Obese): ")
     st.write("Sports drinks, soda, juice, and sweet tea are examples of sugary beverages that have little to no nutritional benefit and add extra calories. Overweight is more common among those who use sugary drinks on a daily basis. Most of the time, go for water or low-fat milk.")
     st.write("Frequent exercise helps you look and feel good and can help you lose weight since it burns calories and builds muscle. Cycling to school, walking the family dog, and engaging in other activities that boost your daily activity level can have an impact. Increase the intensity of your workout and incorporate some muscle-building strength exercises if you want to burn more calories.")
     st.write("The likelihood of being overweight is higher for those who spend a lot of time in front of screens. Limit how much time you spend on non-school-related activities on computers, phones, and tablets, as well as how much time you spend playing video games and watching TV. For a better night's sleep, turn off all screens at least an hour before bed.")
